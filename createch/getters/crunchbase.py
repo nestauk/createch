482bc24ee63726da2490db16269cf8a9e64d7fac
bishax <alex.bishop@nesta.org.uk>
2021-05-26 12:30:18 +0100

    Fix: Removed extra GTR_PATH args to fetch_daps_table

--- createch/getters/crunchbase.py
+++ createch/getters/crunchbase.py
@@ -1,17 +1,19 @@
 import json
 import logging
-import os
-
+from functools import lru_cache
+from typing import Dict
 
 import pandas as pd
+from metaflow import namespace, Run
 
+import createch
 from createch import PROJECT_DIR
-from createch.getters.daps import fetch_daps_table, save_daps_table
+from createch.pipeline.fetch_daps1_data.cb_utils import CB_PATH
 
-CB_PATH = f"{PROJECT_DIR}/inputs/data/crunchbase"
+logger = logging.getLogger(__name__)
+namespace(None)
 
-if os.path.exists(CB_PATH) is False:
-    os.makedirs(CB_PATH)
+RUN_ID: int = createch.config["flows"]["nesta"]["run_id"]
 
 
 def get_crunchbase_orgs():
@@ -39,48 +41,11 @@ def get_crunchbase_vocabulary():
         return json.load(infile)
 
 
-def filter_uk(table: pd.DataFrame, ids: set, var_name: str = "org_id"):
-    """Gets UK companies from crunchbase
-    Args:
-        table: crunchbase table
-        ids: UK company ids
-        var_name: name of org id variable
-    Returns:
-        filtered table
-    """
-    return table.loc[table[var_name].isin(ids)].reset_index(drop=True)
+@lru_cache()
+def _flow(run_id: int) -> Run:
+    return Run(f"CreatechNestaGetter/{run_id}")
 
 
-def fetch_save_crunchbase():
-    """Fetch and save crunchbase data"""
-    cb_orgs = pd.concat(fetch_daps_table("crunchbase_organizations", fields="all"))
-
-    cb_uk = cb_orgs.loc[cb_orgs["country"] == "United Kingdom"].drop_duplicates(
-        subset=["id"]
-    )
-    logging.info(len(cb_uk))
-    save_daps_table(cb_uk, "crunchbase_organisations", CB_PATH)
-
-    cb_uk_ids = set(cb_uk["id"])
-
-    cb_funding_rounds = pd.concat(
-        fetch_daps_table("crunchbase_funding_rounds", fields="all")
-    )
-    cb_funding_rounds_uk = filter_uk(cb_funding_rounds, cb_uk_ids)
-
-    cb_orgs_cats = pd.concat(
-        fetch_daps_table("crunchbase_organizations_categories", fields="all")
-    )
-    cb_org_cats_uk = filter_uk(cb_orgs_cats, cb_uk_ids, "organization_id")
-
-    category_group = pd.concat(
-        fetch_daps_table("crunchbase_category_groups", fields="all")
-    )
-
-    save_daps_table(cb_funding_rounds_uk, "crunchbase_funding_rounds", CB_PATH)
-    save_daps_table(cb_org_cats_uk, "crunchbase_organizations_categories", CB_PATH)
-    save_daps_table(category_group, "crunchbase_category_groups", CB_PATH)
-
-
-if __name__ == "__main__":
-    fetch_save_crunchbase()
+def get_name() -> Dict[str, str]:
+    """Lookup between Crunchbase organisation ID and name."""
+    return _flow(RUN_ID).data.crunchbase_names
--- createch/getters/gtr.py
+++ createch/getters/gtr.py
@@ -1,30 +1,21 @@
 # Fetch GtR tables
 import json
 import logging
-import os
-from typing import Iterator
+from functools import lru_cache
+from typing import Dict
 
 import pandas as pd
-from data_getters.core import get_engine
+from metaflow import namespace, Run
 
+import createch
 from createch import config, PROJECT_DIR
-from createch.getters.daps import (
-    fetch_daps_table,
-    MYSQL_CONFIG,
-    stream_df_to_csv,
-)
+from createch.pipeline.fetch_daps1_data.gtr_utils import GTR_PATH
 
-GTR_PATH = os.path.join(PROJECT_DIR, "inputs/data/gtr")
+logger = logging.getLogger(__name__)
 
-PROJECT_FIELDS = [
-    "id",
-    "title",
-    "grantCategory",
-    "leadFunder",
-    "abstractText",
-    "techAbstractText",
-]
-FUNDER_FIELDS = ["id", "start", "category", "amount"]
+namespace(None)
+
+RUN_ID: int = createch.config["flows"]["nesta"]["run_id"]
 
 
 def get_gtr_projects():
@@ -92,60 +83,11 @@ def get_organisations():
     return orgs
 
 
-def projects_funded_from_2006() -> Iterator[pd.DataFrame]:
-    """GtR projects with funding starting from 2006.
-
-    Returns:
-        Iterable of query results
-    """
-    engine = get_engine(MYSQL_CONFIG)
-    con = engine.connect().execution_options(stream_results=True)
-    query = """
-    SELECT
-        DISTINCT gtr_projects.id AS project_id,
-        gtr_projects.title,
-        gtr_projects.grantCategory,
-        gtr_projects.leadFunder,
-        gtr_projects.abstractText,
-        gtr_projects.potentialImpact,
-        gtr_projects.techAbstractText,
-        gtr_funds.start
-    FROM
-        gtr_projects
-            INNER JOIN
-                (SELECT * FROM gtr_link_table
-                 WHERE gtr_link_table.table_name = 'gtr_funds')
-                AS l
-                ON l.project_id = gtr_projects.id
-            INNER JOIN
-                (SELECT * FROM gtr_funds
-                 WHERE YEAR(gtr_funds.start) > 2006)
-                AS gtr_funds
-                ON l.id = gtr_funds.id
-    GROUP BY gtr_projects.id HAVING MIN(YEAR(gtr_funds.start));
-    """
-
-    return pd.read_sql_query(query, con, chunksize=1000)
+@lru_cache()
+def _flow(run_id: int) -> Run:
+    return Run(f"CreatechNestaGetter/{run_id}")
 
 
-def fetch_save_gtr_tables():
-
-    funders = fetch_daps_table("gtr_funds")
-    stream_df_to_csv(funders, path_or_buf=f"{GTR_PATH}/gtr_funds.csv", index=False)
-
-    topics = fetch_daps_table("gtr_topic")
-    stream_df_to_csv(topics, f"{GTR_PATH}/gtr_topics.csv", index=False)
-
-    link = fetch_daps_table("gtr_link_table")
-    stream_df_to_csv(link, f"{GTR_PATH}/gtr_link_table.csv", index=False)
-
-    logging.info("Filtering projects...")
-    projects_filtered = projects_funded_from_2006()
-    stream_df_to_csv(projects_filtered, f"{GTR_PATH}/gtr_projects.csv", index=False)
-
-
-if __name__ == "__main__":
-    if os.path.exists(GTR_PATH) is False:
-        os.mkdir(GTR_PATH)
-
-    fetch_save_gtr_tables()
+def get_name() -> Dict[str, str]:
+    """Lookup between GtR organisation ID and name."""
+    return _flow(RUN_ID).data.gtr_names
