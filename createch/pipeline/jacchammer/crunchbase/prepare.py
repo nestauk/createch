a179655b5cc7a93236aa0eba5597e677abb9ef92
bishax <alex.bishop@nesta.org.uk>
2021-05-28 18:11:03 +0100

    FIX: ensure outputs/.cache exists before matching.

--- createch/pipeline/jacchammer/crunchbase/prepare.py
+++ createch/pipeline/jacchammer/crunchbase/prepare.py
@@ -4,6 +4,7 @@ Converts Companies House and Crunchbase name data to JSON files required
 by `JacchammerFlow`.
 """
 import json
+from pathlib import Path
 
 from createch import PROJECT_DIR
 from createch.getters.companies_house import get_name as get_ch
@@ -11,7 +12,10 @@ from createch.getters.crunchbase import get_name as get_cb
 
 
 if __name__ == "__main__":
-    with open(f"{PROJECT_DIR}/outputs/.cache/company_names.json", "w") as f:
+    cache_dir = Path(f"{PROJECT_DIR}/outputs/.cache")
+    cache_dir.mkdir(exist_ok=True)
+
+    with open(cache_dir / "company_names.json", "w") as f:
         json.dump(
             get_ch()[["company_number", "name"]]
             .dropna()
@@ -22,5 +26,5 @@ if __name__ == "__main__":
             f,
         )
 
-    with open(f"{PROJECT_DIR}/outputs/.cache/cb_names.json", "w") as f:
+    with open(cache_dir / "cb_names.json", "w") as f:
         json.dump(get_cb(), f)
--- createch/pipeline/jacchammer/gtr/prepare.py
+++ createch/pipeline/jacchammer/gtr/prepare.py
@@ -4,6 +4,7 @@ Converts Companies House and GtR name data to JSON files required
 by `JacchammerFlow`.
 """
 import json
+from pathlib import Path
 
 from createch import PROJECT_DIR
 from createch.getters.companies_house import get_name as get_ch
@@ -11,7 +12,10 @@ from createch.getters.gtr import get_name as get_gtr
 
 
 if __name__ == "__main__":
-    with open(f"{PROJECT_DIR}/outputs/.cache/company_names.json", "w") as f:
+    cache_dir = Path(f"{PROJECT_DIR}/outputs/.cache")
+    cache_dir.mkdir(exist_ok=True)
+
+    with open(cache_dir / "company_names.json", "w") as f:
         json.dump(
             get_ch()[["company_number", "name"]]
             .dropna()
@@ -22,5 +26,5 @@ if __name__ == "__main__":
             f,
         )
 
-    with open(f"{PROJECT_DIR}/outputs/.cache/gtr_names.json", "w") as f:
+    with open(cache_dir / "gtr_names.json", "w") as f:
         json.dump(get_gtr(), f)
