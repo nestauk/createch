cbc68be2c9f7ff21ca83dbf5ec7de56b413873f3
Juan-Mateos <juan.mateos-garcia@nesta.org.uk>
2021-05-31 12:53:39 +0100

    added extra getters to gtr getter script

--- createch/getters/gtr.py
+++ createch/getters/gtr.py
@@ -6,7 +6,7 @@ import os
 
 import pandas as pd
 
-from createch import PROJECT_DIR
+from createch import config, PROJECT_DIR
 from createch.getters.daps import fetch_daps_table, save_daps_table
 
 GTR_PATH = os.path.join(PROJECT_DIR, "inputs/data/gtr")
@@ -43,6 +43,48 @@ def get_gtr_tagged():
 def get_gtr_vocabulary():
     with open(f"{PROJECT_DIR}/outputs/data/gtr/gtr_vocabulary.json", "r") as infile:
         return json.load(infile)
+
+
+def get_cis_lookup():
+    lookup = pd.read_csv(
+        f"{PROJECT_DIR}/inputs/data/sic_sector_lookup.csv", dtype={"SIC4_code": str}
+    )
+    code_industry_lu = (
+        lookup.query("industry != 'Life Sciences'")
+        .set_index("SIC4_code")["industry"]
+        .to_dict()
+    )
+    return code_industry_lu
+
+
+def get_link_table():
+    return pd.read_csv(f"{PROJECT_DIR}/inputs/data/gtr/gtr_link_table.csv")
+
+
+def get_organisations():
+
+    SIC_IND_LOOKUP = get_cis_lookup()
+    FILTER_TERMS = config["gtr_organisations"]["filter_terms"]
+
+    orgs = pd.read_csv(
+        "~/Desktop/projects/im-minerva/data/processed/gtr/projects_organisations.csv",
+        dtype={"SIC4_code": str},
+    )
+
+    orgs = orgs[["id", "name", "company_number", "score", "SIC4_code"]].drop_duplicates(
+        "id"
+    )
+    orgs["flag"] = [
+        any(x in name.lower() for x in FILTER_TERMS) for name in orgs["name"]
+    ]
+
+    orgs = (
+        orgs.query("flag==False")
+        .assign(creative_sector=lambda df: df["SIC4_code"].map(SIC_IND_LOOKUP))
+        .dropna(axis=0, subset=["creative_sector"])
+        .reset_index(drop=True)
+    )
+    return orgs
 
 
 def filter_projects(
