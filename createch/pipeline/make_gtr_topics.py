4a615b0b69344f5a42e99e693e51029dc0620a61
Juan-Mateos <juan.mateos-garcia@nesta.org.uk>
2021-05-31 12:39:10 +0100

    added script to identify ci projects and train topic model on them

---
+++ createch/pipeline/make_gtr_topics.py
@@ -0,0 +1,52 @@
+# Imports
+import logging
+from numpy.random import seed
+import pandas as pd
+
+from createch import PROJECT_DIR, config
+from createch.getters.gtr import (
+    get_link_table,
+    get_cis_lookup,
+    get_gtr_projects,
+    get_organisations,
+)
+from createch.pipeline.topic_modelling import train_model, post_process_model_clusters
+from createch.utils.io import save_lookup
+
+# Functions
+
+
+def make_creative_tokenised(projects, orgs, tokenised_descrs):
+
+    logging.info("Merging tables")
+    link_table = get_link_table().query("table_name == 'gtr_organisations'")
+
+    project_org = projects.merge(
+        link_table, left_on="project_id", right_on="project_id"
+    ).merge(orgs, on="id")
+    ci_projects = set(project_org["project_id"])
+
+    logging.info("Getting tokenised")
+    ci_tokenised = {k: v for k, v in get_gtr_tokenised().items() if k in ci_projects}
+    return ci_tokenised
+
+
+if __name__ == "__main__":
+
+    seed(999)
+    logging.info("Reading data")
+
+    SIC_IND_LOOKUP = get_cis_lookup()
+    projects = get_gtr_projects()
+    ci_orgs = get_organisations()
+
+    ci_tokenised = make_creative_tokenised(projects, ci_orgs, descr_tokenised)
+
+    logging.info("Training topic model")
+    gtr_topsbm = train_model(list(ci_tokenised.values()), list(ci_tokenised.keys()))
+
+    topic_mix_df, clusters = post_process_model_clusters(
+        gtr_topsbm, top_level=0, cl_level=1
+    )
+
+    topic_mix_df.to_csv(f"{PROJECT_DIR}/outputs/data/gtr/gtr_topic_mix.csv")
