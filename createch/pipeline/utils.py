f20a794d174009504e9a7ee5b561b2da66c1e5a7
Juan-Mateos <juan.mateos-garcia@nesta.org.uk>
2021-06-06 13:39:10 +0100

    added utility function to combine crunchbase short and long descriptions

--- createch/pipeline/utils.py
+++ createch/pipeline/utils.py
@@ -1,4 +1,16 @@
 # Various utilities that we use across the pipeline
+import pandas as pd
+
+
+def make_org_description_lu(orgs):
+
+    descr = {
+        r["id"]: r["long_description"]
+        if pd.isnull(r["long_description"]) == True
+        else r["short_description"]
+        for _, r in orgs.iterrows()
+    }
+    return descr
 
 
 def has_createch_sector(comm_number, vector, comm_container, thres=0.04):
